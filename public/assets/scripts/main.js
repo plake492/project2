2595ac2959d040fe60446a7f69fbea6d5cd44362
plake <plake492@gmail.com>
2019-09-18 21:31:41 -0400

    testing geolocation/ added sequelize-cli

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,54 +1,47 @@
-let lat = ""
-let lng = ""
+let searchLat = "";
+let searchLng = "";
+
+let currentLat = "";
+let currentLng = "";
+
+const key = "G3w5wY8BZGgo9lwNEdX00o8Wvnb1xAf6";
+
 
 $("#addressForm").on("submit", function(event) {
     event.preventDefault();
     $("#addresLocation").empty();
     $("#map").empty();
-
-  
-    const key = "G3w5wY8BZGgo9lwNEdX00o8Wvnb1xAf6";
     const address = $("#addres").val().trim();
     const queryUrl = `https://www.mapquestapi.com/geocoding/v1/address?key=${key}&inFormat=kvp&outFormat=json&location=${address}&thumbMaps=false`
-
 
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(response => {
         const result = response.results[0].locations[0].latLng;
-        console.log(result)
 
-        lat = result.lat
-        lng = result.lng
+        searchLat = result.lat
+        searchLng = result.lng
         $("#addresLocation").append(`
-            <hr>
-                <p>The coordinates of ${address} are</p>
-            <br>
-                <p>Latitude: ${result.lat}</p> 
-            <br>
-                <p>Longitude: ${result.lng}</p>
+            <hr><p>The coordinates of ${address} are</p>
+            <br><p>Latitude: ${result.lat}</p> 
+            <br><p>Longitude: ${result.lng}</p>
         `);
         
-        const queryUrl2 = `https://open.mapquestapi.com/staticmap/v5/map?key=${key}&locations=${lat},${lng}&size=250,250@2x`
+        const queryUrl2 = `https://open.mapquestapi.com/staticmap/v5/map?key=${key}&locations=${searchLat},${searchLng}&size=250,250@2x`;
         
         $.ajax({
             url: queryUrl2,
             method: "GET"
         }).then(response => {
 
-            console.log("MAP " + response)
-            console.log(queryUrl2)
             $("#map").append(`
                 <img src="${queryUrl2}">
             `);
-        })
-
-        getLocation()
-    })
-
-
-})
+            getLocation();
+          });
+    });
+});
 
 
 function getLocation() {
@@ -56,17 +49,24 @@ function getLocation() {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
         $("#currentLocationDisplay").text("Geolocation is not supported by this browser.");
-    }
-}
-
+    };
+};
 function showPosition(position) {
-    $("currentLocationDisplay").empty();
+    $("#currentLocationDisplay").empty();
+    currentLat = position.coords.latitude;
+    currentLng = position.coords.longitude;
 
+    const distanceFromLat = Math.abs(currentLat - searchLat) 
+    const distanceFromLng = Math.abs(currentLng - searchLng)
+    console.log(distanceFromLat + " : " + distanceFromLng)
+    if (distanceFromLat <= 0.001 && distanceFromLng <= 0.001) {
+         alert("YOUR HERE")
+    };
 
     $("#currentLocationDisplay").append(`
-        <br>
-        <p>Latitude: ${position.coords.latitude} </p> 
-        <br>
-        <p>Longitude: ${position.coords.longitude}</p>
+        <br><p>Latitude: ${currentLat} </p> 
+        <br><p>Longitude: ${currentLng}</p>
     `);
-}
+};
+
+
