0a6a4e1569becff54a6283b2b234b47d4a0544d6
iuglyboi28 <iuglyboi28@gmail.com>
2019-09-21 12:14:58 -0400

    worked on app.js

--- models/project.js
+++ models/project.js
@@ -1,44 +1,43 @@
-module.exports = function (sequelize, DataTypes) {
-    var postProject = sequelize.define("Project", {
-        // id: {
-        //     type: DataTypes.INTEGER,
-        //     allowNull: false,
-        //     autoIncrement: true,
-        //     primaryKey: true
-        // },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            min: 1,
-            max: 100
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [3-300]
-            }
-        },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        peopleNeeded: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        date:{
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        location: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [3-300]
-            }
-        }
-    });
-    return postProject;
-}
-
+module.exports = function(sequelize, DataTypes) {
+  var postProject = sequelize.define("Project", {
+    // id: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: false,
+    //     autoIncrement: true,
+    //     primaryKey: true
+    // },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      min: 1,
+      max: 100
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3 - 300]
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    peopleNeeded: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3 - 300]
+      }
+    }
+  });
+  return postProject;
+};
