a4bd082cde4fbfd3f7ba0ac0dabce22fee35314f
plake <plake492@gmail.com>
2019-09-22 20:34:17 -0400

    added new signup form

--- routes/hbsRoutes.js
+++ routes/hbsRoutes.js
@@ -2,14 +2,14 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Project.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        title: "Welcome!",
-        description: dbExamples
-      });
-    });
-  });
+  // app.get("/", function(req, res) {
+  //   db.Project.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       title: "Welcome!",
+  //       description: dbExamples
+  //     });
+  //   });
+  // });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
@@ -24,7 +24,8 @@ module.exports = function(app) {
 
   // Added by Patrick
   app.get("/", function(req, res) {
-    res.render("index", { list: db.projects });
+    res.render("index", { list: db });
+    console.log(db);
   });
   app.get("/viewpost/:id", function(req, res) {
     res.render("post", db[req.params.id]);
@@ -35,7 +36,9 @@ module.exports = function(app) {
   app.get("/signin", function(req, res) {
     res.render("signin", { list: db });
   });
-
+  app.get("/newprofile", function(req, res) {
+    res.render("newprofile", { list: db });
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
