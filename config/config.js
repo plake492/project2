9355fb6417a17a4a5741307e792df2e82d9f1ea7
iuglyboi28 <iuglyboi28@gmail.com>
2019-09-26 20:15:36 -0400

    cleaning up passport js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    host: "localhost",
     dialect: "mysql"
   },
   test: {
--- server.js
+++ server.js
@@ -1,62 +1,86 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
 
+var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
+// Creating express app and configuring middleware needed for authentication
 var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Requiring our routes
+require("./routes/hbsRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
-//! JUST FOR TESTING
-  // const posts = require("./testObject")
-
-  // app.get("/", function(req, res) {
-  //   res.render("index", { list: posts });
-  // });
-  // app.get("/viewpost/:id", function(req, res) {
-  //   res.render("post", posts[req.params.id]);
-  // });
-  // app.get("/createproject", function(req, res) {
-  //   res.render("createproject", { list: posts });
-  // });
-//! JUST FOR TESTING
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/hbsRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
 
-module.exports = app;
+// var PORT = process.env.PORT || 3000;
+
+// // Middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.static("public"));
+
+
+// //! JUST FOR TESTING
+//   // const posts = require("./testObject")
+
+//   // app.get("/", function(req, res) {
+//   //   res.render("index", { list: posts });
+//   // });
+//   // app.get("/viewpost/:id", function(req, res) {
+//   //   res.render("post", posts[req.params.id]);
+//   // });
+//   // app.get("/createproject", function(req, res) {
+//   //   res.render("createproject", { list: posts });
+//   // });
+// //! JUST FOR TESTING
+
+// // Handlebars
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
+
+// // Routes
+// require("./routes/apiRoutes")(app);
+// require("./routes/hbsRoutes")(app);
+
+// var syncOptions = { force: false };
+
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
+
+// // Starting the server, syncing our models ------------------------------------/
+// db.sequelize.sync(syncOptions).then(function() {
+//   app.listen(PORT, function() {
+//     console.log(
+//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+//       PORT,
+//       PORT
+//     );
+//   });
+// });
+
+// module.exports = app;
